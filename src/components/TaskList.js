998a7defb0357c42409ab8cce392c8e9feda0103
Michael Cruz <cmichael_12@yahoo.com>
2023-08-20 09:42:31 -0500

    nc1

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task"
+import { TASKS } from "../data";
+
 
 function TaskList() {
+  const [tasks, setTasks]=useState(TASKS)
+
+  function handleDeleteClick(taskId){
+    setTasks(tasks.filter(t=>t.text!==taskId )
+    )
+  }
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+    <ul>
+    {tasks.map(task=>(
+      < Task category={task.category} text={task.text}
+      onDelete={handleDeleteClick} />
+    ))
+    }
+   </ul>
     </div>
   );
 }
